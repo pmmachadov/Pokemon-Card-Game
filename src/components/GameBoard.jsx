0b6ed3b9ec2e2327dab5172cc4401ce1d191d78c
Pablo Machado <100440808+pmmachadov@users.noreply.github.com>
2024-08-28 00:18:43 +0200

    CSS update on scoreboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,17 +2,14 @@ import { useState, useEffect } from 'react';
 import { fetchPokemon } from '../api/pokemonApi';
 import Card from './Card';
 import ScoreBoard from './ScoreBoard';
-import '../styles/styles.css';
+
+import '../styles/styles.css'
 
 const GameBoard = () => {
     const [cards, setCards] = useState([]);
     const [currentScore, setCurrentScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
     const [selectedCards, setSelectedCards] = useState([]);
-
-    // Crear instancias de Audio para cada sonido
-    const winSound = new Audio('/sounds/win.wav');
-    const loseSound = new Audio('/sounds/lose.wav');
 
     useEffect(() => {
         const initializeGame = async () => {
@@ -37,23 +34,15 @@ const GameBoard = () => {
 
     const handleCardClick = (id) => {
         if (selectedCards.includes(id)) {
-            loseSound.play().catch((error) => {
-                console.error("Error playing lose sound:", error);
-            }); // Reproducir sonido de derrota
             setCurrentScore(0);
             setSelectedCards([]);
         } else {
-            winSound.play().catch((error) => {
-                console.error("Error playing win sound:", error);
-            }); // Reproducir sonido de victoria
             setCurrentScore(currentScore + 1);
             setSelectedCards([...selectedCards, id]);
             if (currentScore >= bestScore) {
                 setBestScore(currentScore + 1);
             }
         }
-
-        // Animación de volteo de la carta
         setCards(cards.map(card => card.id === id ? { ...card, flipped: !card.flipped } : card));
         setCards(shuffleCards([...cards]));
     };
